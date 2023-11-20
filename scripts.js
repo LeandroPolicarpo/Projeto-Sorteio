8565519608eb708b942bc8f05dbf71d2bdb5a0d9
leandropolicarpo <leandrogadelhapolicarpo@gmail.com>
2023-11-19 21:52:37 -0300

    sorteio

---
+++ scripts.js
@@ -0,0 +1,47 @@
+function generateNumber() {
+
+    const min= Math.ceil(document.querySelector(".input-min").value)
+    const max= Math.floor(document.querySelector(".input-max").value)
+
+
+
+    if (min > max)
+    alert ("o valor minimo tem que ser MENOR que valor máximo")
+
+    else {
+        const result = Math.floor(Math.random() * (max - min + 1)) + min
+
+        alert( result)
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
